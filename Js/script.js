9a9492de12508c491bf2aecc6547e2b842566cca
Rain <user@localhost.localdomain>
2020-10-25 01:17:57 -0400

    finally

--- Js/script.js
+++ Js/script.js
@@ -138,57 +138,27 @@ var aText = new Array(
           }
         });
     
-          // -------   Mail Send ajax
-    
-             $(document).ready(function() {
-                var form = $('#myForm'); // contact form
-                var submit = $('.submit-btn'); // submit button
-                var alert = $('.alert-msg'); // alert div for show alert message
-    
-                // form submit event
-                form.on('submit', function(e) {
-                    e.preventDefault(); // prevent default form submit
-    
-                    $.ajax({
-                        url: 'mail.php', // form action url
-                        type: 'POST', // form submit method get/post
-                        dataType: 'html', // request type html/json/xml
-                        data: form.serialize(), // serialize form data
-                        beforeSend: function() {
-                            alert.fadeOut();
-                            submit.html('Sending....'); // change submit button text
-                        },
-                        success: function(data) {
-                            alert.html(data).fadeIn(); // fade in response data
-                            form.trigger('reset'); // reset form
-                            submit.attr("style", "display: none !important");; // reset submit button text
-                        },
-                        error: function(e) {
-                            console.log(e)
-                        }
-                    });
-                });
-            });
-    
-    
-     });
-    
-    
-     mybutton = document.getElementById("myBtn");
-    
-    // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function() {scrollFunction()};
-    
-    function scrollFunction() {
-      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-      } else {
-        mybutton.style.display = "none";
-      }
-    }
-    
-    // When the user clicks on the button, scroll to the top of the document
-    function topFunction() {
-      document.body.scrollTop = 0; // For Safari
-      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-    }
+        function _(id){ return document.getElementById(id); }
+        function submitForm(){
+          _("button").disabled = true;
+          _("status").innerHTML = 'please wait...';
+          var formdata = new FormData();
+          formdata.append( "firstName", _("firstName").value );
+          formdata.append( "lastName", _("lastName").value );
+          formdata.append( "phoneNumber", _("phoneNumber").value );
+          formdata.append( "emailAddress", _("emailAddress").value );
+          formdata.append( "message", _("message").value );
+          var ajax = new XMLHttpRequest();
+          ajax.open( "POST", "mail.php" );
+          ajax.onreadystatechange = function() {
+            if(ajax.readyState == 4 && ajax.status == 200) {
+              if(ajax.responseText == "success"){
+                _("my_form").innerHTML = '<h2>Thank you '+_("name").value +', your message has been sent.</h2>';
+              } else {
+                _("status").innerHTML = ajax.responseText;
+                _("button").disabled = false;
+              }
+            }
+          }
+          ajax.send( formdata );
+        }
--- mail.php
+++ mail.php
@@ -1,23 +1,23 @@
 <?php
 
 
-   $name = $_POST['name'];
-   $subject = $_POST['subject'];
-   $visitor_email = $_POST['email'];
-   $message = $_POST['message'];
-
-   $email_from = 'worksonme313@gmail.com';
-   $email_subject = 'New Form Submission';
-   $email_body = "User Name: $name.\n";
-				 "User Email:$visitor_email\n";
-				 "User subject:$subject\n";
-				 "User Message:$message\n";
-   $to = "lorrainekamanda@gmail.com";
-
-   $headers = "From: $email_from\r\n";
-   $headers .= "Reply-To: ". $visitor_email."\r\n";
-
-   mail($to, $email_subject, $email_body, $headers)
-   
-
+if( isset($_POST['name']) && isset($_POST['lastName']) && isset($_POST['phoneNumber']) && isset($_POST['emailAddress']) && isset($_POST['message']) ){
+	$name = $_POST['name'];
+  $lastName = $_POST['lastName'];
+  $phoneNumber = $_POST['phoneNumber'];
+	$emailAddress = $_POST['emailAddress'];
+	$message = nl2br($_POST['message']);
+	$to = 'lorrainekamanda@gmail.com';
+	$from = $emailAddress;
+	$subject = 'Schedule Inquiry';
+	$message = "<b>Name:</b> '.$name.' <b>LastName:</b> '.$lastName.' <br><b>Phone:</b> '.$phoneNumber' <b>Email:</b> '.$emailAddress.' <p>'.$message.'</p>";
+	$headers = 'From: $from\n';
+	$headers .= 'MIME-Version: 1.0\n';
+	$headers .= 'Content-type: text/html; charset=iso-8859-1\n';
+	if( mail($to, $subject, $message, $headers) ){
+		echo "success";
+	} else {
+		echo "The server failed to send the message. Please try again later.";
+	}
+}
 ?>
